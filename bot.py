4553fed3317c70c0d9347fdb4a3441462bce81bd
vermont <d.l.ermolaev@yandex.ru>
2020-10-11 23:32:00 +0300

    добавил приветствие

--- bot.py
+++ bot.py
@@ -7,10 +7,12 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
+markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
 @dp.message_handler(commands='start')
 async def welcome(message: types.Message):
-    await message.reply('Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот для быстрого заказа обеда из '
-                    'кафе Шарлотка. '.format(message.from_user, bot.get_me()), parse_mode='html')
+    await message.reply(f'Добро пожаловать, {message.from_user.first_name}!\nЯ - <b>SharlotkaTestBot</b>, бот для быстрого заказа обеда из '
+                    'кафе Шарлотка.', parse_mode='html', reply_markup=markup)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
